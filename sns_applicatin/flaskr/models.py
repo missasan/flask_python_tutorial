048f79ad2412888375e5387b3e8794748bd7ac2a
missasan <marukuteiimono@gmail.com>
2021-07-30 09:05:06 +0900

    section9 132完了

---
+++ sns_applicatin/flaskr/models.py
@@ -0,0 +1,44 @@
+from flaskr import db, login_manager
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from datetime import datetime, timedelta
+from uuid import uuid4
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(UserMixin, db.Model):
+
+    __tablename__ == 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(
+        db.String(128),
+        default=generate_password_hash('snsflaskapp')
+    )
+    picture_path = db.Column(db.Text)
+    # 有効か無効かのフラグ
+    is_active = db.Column(db.Boolean, unique=False, default=False)
+    create_at = db.Column(db.DateTime, default=datetime.now)
+    update_at = db.Column(db.DateTime, default=datetime.now)
+
+
+class PasswordResetToken(db.Model):
+
+    __tablename__ = 'password_reset_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(
+        db.String(64),
+        unique=True,
+        index=True,
+        serve_default=str(uuid4)
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    expire_at = db.Column(db.DateTime, default=datetime.now)
+    create_at = db.Column(db.DateTime, default=datetime.now)
+    update_at = db.Column(db.DateTime, default=datetime.now)
