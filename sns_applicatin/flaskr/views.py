4a2447ce3e1c3cc198c80e9b85eb92a2f2e3430b
missasan <marukuteiimono@gmail.com>
2021-07-31 11:06:43 +0900

    section9 133完了

---
+++ sns_applicatin/flaskr/views.py
@@ -0,0 +1,28 @@
+from flask import (
+    Blueprint, abort, request, render_template,
+    redirect, url_for, flash
+)
+from flask_login import login_user, login_required, logout_user
+from flaskr.models import (
+    User, PasswordResetToken
+)
+from flaskr import db
+
+from flaskr.forms import (
+    LoginForm, RegisterForm
+)
+
+bp = Blueprint('app', __name__, url_prefix='')
+
+@bp.route('/')
+def home():
+    return render_template('home.html')
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('app.home'))
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
