c4dc2083fd1fcf1a02c30a3052fada1531a820cd
missasan <marukuteiimono@gmail.com>
2021-08-01 10:31:32 +0900

    section9 134完了

--- sns_applicatin/flaskr/views.py
+++ sns_applicatin/flaskr/views.py
@@ -3,6 +3,7 @@ from flask import (
     redirect, url_for, flash
 )
 from flask_login import login_user, login_required, logout_user
+from wtforms.fields.simple import PasswordField
 from flaskr.models import (
     User, PasswordResetToken
 )
@@ -26,3 +27,41 @@ def logout():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user User.select_user_by_email(form.email.data)
+        if user and user.is_active and user.validate_password(form.password.data):
+            login_user(user, remember=True)
+            next = request.args.get('next')
+            if not next:
+                next = url_for('app.home')
+            return redirect(next)
+        elif not user:
+            flash('存在しないユーザーです')
+        elif not user.is_active:
+            flash('無効なユーザーです。パスワードを再設定してください')
+        elif not user.validate_password(form.password.data):
+            flash('メールアドレスとパスワードの組み合わせが誤っています')
+    return render_template('login.html', form=form)
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(
+            username = form.username.data,
+            email = form.email.data
+        )
+        with db.session.begin(subtransaction=True):
+            user.create_new_user()
+        db.session.commit()
+        token = ''
+        with db.session.begin(subtransactions=True):
+            token = PasswordResetToken.publish_token(user)
+        db.session.commit()
+        # メールに飛ばすほうがいい
+        print(
+            f'パスワード設定用URL: http://127.0.0.1:5000/reset_password/{token}'
+        )
+        flash('パスワード設定用のURLをお送りしました。ご確認ください')
+        return redirect(url_for('app.login'))
+    return render_template('register.html', form=form)
